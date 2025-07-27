fcc53ba73e1aed936fa532cbcd17db5b0ad9472c
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-07-27 16:05:24 -0400

    audio: Use audio_effects.xml in local device tree
    This audio/audio_effects.xml was derived from https://github.com/yaap/hardware_qcom_audio-ar-sm8650/blob/sixteen/configs/pineapple/audio_effects.xml at 8c56c27c677c716ab217b85004880693df0b8923

--- common.mk
+++ common.mk
@@ -93,6 +93,9 @@ PRODUCT_COPY_FILES += \
 # Custom audio_effects.xml
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_pineapple/audio_effects.xml
 
+# Custom audio_effects.xml
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_pineapple/audio_effects.xml
+
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
