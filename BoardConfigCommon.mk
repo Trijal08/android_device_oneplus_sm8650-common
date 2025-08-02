8e815ecc12e0847369cf40513f951867c6b02254
Trijal Saha <trijalsaha2012@gmail.com>
2025-08-02 02:48:29 -0400

    Custom touch HIDL compatibiity matrix.xml

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -82,6 +82,10 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/oplus/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/yaap/config/device_framework_matrix.xml
+
+# Touch HIDL custom matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(COMMON_PATH)/touch-hidl_matrix.xml
+
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/framework_manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := \
